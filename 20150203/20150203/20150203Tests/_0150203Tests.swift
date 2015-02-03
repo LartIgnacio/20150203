7f396d60eb598982a8f22694db64b2d939807a90
LartIgnacio <ace.ignacio@gmail.com>
2015-02-03 22:09:17 +0800

    Create a spritekit project

---
+++ 20150203/20150203/20150203Tests/_0150203Tests.swift
@@ -0,0 +1,36 @@
+//
+//  _0150203Tests.swift
+//  20150203Tests
+//
+//  Created by Lart Ace Ignacio on 2/3/15.
+//  Copyright (c) 2015 Lart Ace Ignacio. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class _0150203Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
