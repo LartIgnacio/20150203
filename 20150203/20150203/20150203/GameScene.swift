4072654a79116ae90eb47ed6ed19e38532814973
LartIgnacio <ace.ignacio@gmail.com>
2015-02-05 23:06:00 +0800

    Implement game state and initial commit for initPlayer

--- 20150203/20150203/20150203/GameScene.swift
+++ 20150203/20150203/20150203/GameScene.swift
@@ -8,6 +8,25 @@
 
 import SpriteKit
 
+// GameState
+enum HSGameState: Int {
+    case HSGameStateStart
+    case HSGameStatePlaying
+    case HSGameStateAnimating
+    case HSGameStateEnd
+}
+
+var state:HSGameState     = .HSGameStateStart
+
+// player
+var player:SKSpriteNode   = SKSpriteNode()
+
+// background
+let background:SKNode     = SKNode()
+
+// Floor Height
+var floorDistance:CGFloat = 0.0
+
 class GameScene: SKScene {
     
     required init?(coder aDecoder: NSCoder) {
@@ -16,39 +35,29 @@ class GameScene: SKScene {
     
     override init(size: CGSize) {
         super.init(size: size)
+        
+        floorDistance = size.height * 0.4;
+        
+        self.initPlayer()
     }
     
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
+    
+    func initPlayer() {
+        player = SKSpriteNode(color: UIColor .yellowColor(), size: CGSizeMake(20.0, 20.0))
+        player.position = CGPointMake(20.0, floorDistance);
+        
+        self .addChild(player)
+    }
+    
 }
